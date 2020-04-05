51fa36186712bfbf1d8d9ba539ea45f7112e004c
mbatoul <mathis.batoul@hotmail.fr>
2020-04-05 03:57:33 +0200

    Add target blank to footer link

--- client/src/components/TheFooter.vue
+++ client/src/components/TheFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        Built with <strong>VueJS</strong> and <strong>Ruby-On-Rails</strong> by <a href="https://github.com/mbatoul/" target='_blank'>Mathis Batoul</a>. The <a href='https://github.com/mbatoul/coviz19'>source code</a> is licensed
+        Built with <strong>VueJS</strong> and <strong>Ruby-On-Rails</strong> by <a href="https://github.com/mbatoul/" target='_blank'>Mathis Batoul</a>. The <a href='https://github.com/mbatoul/coviz19' target='_blank'>source code</a> is licensed
         <a href="http://opensource.org/licenses/mit-license.php" target='_blank'>MIT</a>.
       </p>
     </div>
